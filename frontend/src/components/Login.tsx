5bfff71b20abf91cd805933880d8c913ef663c1f
Cian McKenna <cian.mck20@gmail.com>
2024-10-20 19:11:08 +0100

    Login Page Dev

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,7 +1,29 @@
+import '../App.css';
+import { Box } from '@mui/system';
+import MyTextField from './forms/MyTextField';
+import MyPassField from './forms/MyPassField';
+
 const Login = () => {
     return (
-        <div>
-            <h1>Login Page</h1>
+        <div className={"myBackground"}>
+            <Box className={"whiteBox"}>
+                <Box className={"itemBox"}>
+                    <Box className={"title"}>Login for Airgead Planner</Box>
+                </Box>
+                <Box className={"itemBox"}>
+                    <MyTextField label={"Email"} />
+                </Box>
+                <Box className={"itemBox"}>
+                    <MyPassField label={"Password"} />
+                </Box>
+                <Box className={"itemBox"}>
+                    Submit Button
+                </Box>
+                <Box className={"itemBox"}>
+                    Link to Register Page
+                </Box>
+                
+            </Box>
         </div>
     );
 }
