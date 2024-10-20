ddfbb3b851033166d03de69140f1ae1c4adf9009
Cian McKenna <cian.mck20@gmail.com>
2024-10-20 18:20:56 +0100

    Custom User Created

--- backend/dashboard/settings.py
+++ backend/dashboard/settings.py
@@ -58,6 +58,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
 ]
 
+AUTH_USER_MODEL = 'users.CustomUser'
+
 ROOT_URLCONF = 'dashboard.urls'
 
 TEMPLATES = [
@@ -91,7 +93,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '5432',
         'OPTIONS': {
-            'options': '-c search_path=test_data'
+            'options': '-c search_path=airgead_planner'
         }
     }
 }
