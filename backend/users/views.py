8355994bb161ec55fa0ca6be1e87e74ebc8b9e38
Cian McKenna <cian.mck20@gmail.com>
2024-10-22 17:58:43 +0100

    Register Viewsets

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .serializers import *
+from .models import *
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
-# Create your views here.
+class RegisterViewset(viewsets.ViewSet):
+    permission_classes = [permissions.AllowAny]
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+    def create(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        else:
+            return Response(serializer.errors, status=400)
